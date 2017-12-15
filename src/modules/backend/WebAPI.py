ebf8fe42ce673228718acf1fecf73890becea32e
oferh <ofer.handel@gmail.com>
2017-12-15 11:33:38 +0200

    graph register

--- src/modules/backend/WebAPI.py
+++ src/modules/backend/WebAPI.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, jsonify
+from flask import flash
 
 from modules.dal.GraphConnection import bolt_connect
 from src.modules import Logger
 from src.modules.backend.APIConstants import *
-from src.modules.dal.graphObjects.graphObjects import User, Party
-
+from src.modules.dal.graphObjects.graphObjects import User, Party, ElectedOfficial
 
 app = Flask(__name__)
 app.secret_key = "ThisIsNotThePassword"
 logger = Logger.getLogger("WebAPI", is_debug=True)
-
-
-
+graph = bolt_connect()
 
 @app.errorhandler(Exception)
 def defaultHandler(error):
@@ -27,12 +25,20 @@ def isUserInSession(user):
 
 @app.route("/getParties", methods=['GET'])
 def getParties():
-    graph = bolt_connect()
+
     parties = Party.select(graph)
     parties_names = []
     for party in parties:
         parties_names.append(party.name)
     return jsonify(parties_names)
+
+@app.route("/getElectedOfficials", methods=['GET'])
+def getElectedOfficials():
+    elected_officials = ElectedOfficial.select(graph)
+    elected_official_names = []
+    for elected_official in elected_officials:
+        elected_official_names.append(elected_official.name)
+    return jsonify(elected_official_names)
 
 # getResidancies like parties
 
@@ -40,14 +46,8 @@ def getParties():
 
 # adding שמאל and ימין tags
 
-
-
-
 @app.route("/register", methods=['POST'])
 def register():
-    graph = bolt_connect()
-    graph.begin(autocommit=True)
-
     user_token = request.form.get(USER_TOKEN)
     birth_year = request.form.get(BIRTH_YEAR)
     job = request.form.get(JOB)
@@ -59,11 +59,12 @@ def register():
                                involvmentLevel=involvment, residancy=city,
                                job=job)
     new_user.associate_party = Party.select(graph, primary_value=party).first()
-    graph.push(new_user)
-
+    graph.begin(autocommit=True).push(new_user)
     return Response.SUCCESSS, Response.CODE_SUCCESS
 
 # api functions for first time login  --- end
+
+# notification api function --- start
 
 app.run("127.0.0.1", 8080, debug=True)
 
@@ -75,7 +76,7 @@ app.run("127.0.0.1", 8080, debug=True)
 #     return {party members votes - his own party, other parties, etc.}
 #
 #
-# Vonites History Screen
+# Votes History Screen
 # 1. get last 100 laws  -> get user token
 #       return last 100 laws from the relevant severity :for each law {id, link, name, description, party, party member}.
 #       from here he can vote for the law
