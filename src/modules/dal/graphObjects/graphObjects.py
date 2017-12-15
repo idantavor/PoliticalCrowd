ab32e671faab79eca349fff97e1ad4c0f24db02c
ramhillel <ramhillel12@gmail.com>
2017-12-15 13:46:05 +0200

    ttt

--- src/modules/dal/graphObjects/graphObjects.py
+++ src/modules/dal/graphObjects/graphObjects.py
@@ -6,8 +6,8 @@ from src.modules.dal.relations.Relations import VOTED_FOR, VOTED_AGAINST, ELECTE
     ELECTED_ABSTAINED_FIRST, ELECTED_ABSTAINED_SECOND, ELECTED_ABSTAINED_THIRD, ELECTED_MISSING_FIRST, \
     ELECTED_MISSING_SECOND, ELECTED_MISSING_THIRD, ASSOCIATE_PARTY, MEMBER_OF_PARTY, TAGGED_AS, TAGGED_LAW, WORK_AT, \
     RESIDING
-
-from src.modules.dal.GraphConnection import *
+from src.modules.backend.APIConstants import BLANK_TAG
+import datetime
 
 
 class Party(GraphObject):
@@ -77,17 +77,12 @@ class Tag(GraphObject):
 class Votes(object):
     def __init__(self):
         self._upvotes = 0
-        self._downvotes = 0
-
 
     def upvote(self):
         self._upvotes += 1
 
-    def downvote(self):
-        self._downvotes += 1
-
     def getScore(self):
-        return self._upvotes - self._downvotes
+        return self._upvotes
 
 
 
@@ -137,7 +132,16 @@ class Law(GraphObject):
         return law
 
     # def tagVote(self, graph, tag_name, is_upvote=True):
-    #     if tag_name ==
+    #     if tag_name not in self.tags_votes:
+    #         self.tags_votes[tag_name] = Votes()
+    #     if is_upvote:
+    #         self.tags_votes[tag_name].upvote()
+    #     else:
+    #         self.tags_votes[tag_name].downvote()
+    #     tagNode = Tag.select(graph, primary_value=tag_name)
+    #     self.tags.add(tagNode).first()
+
+
 
     def __str__(self, *args, **kwargs):
         return self.__ogm__.node.__str__()
@@ -146,12 +150,14 @@ class Law(GraphObject):
 class JobCategory(GraphObject):
     __primarykey__ = "name"
 
+    name = Property()
     users = RelatedFrom("User", WORK_AT)
 
 
 class Residency(GraphObject):
     __primarykey__ = "name"
 
+    name = Property()
     users = RelatedFrom("User", RESIDING)
 
 
@@ -159,26 +165,68 @@ class User(GraphObject):
     __primarykey__ = "token"
 
     token = Property()
-    birthYear = Property(key="birthYear")
-    involvmentLevel = Property(key="involvmentLevel")
+    birth_year = Property(key="birthYear")
+    involvment_level = Property(key="involvmentLevel")
 
     residency = RelatedTo(Residency)
-    jobCategory = RelatedTo(JobCategory)
+    job_category = RelatedTo(JobCategory)
     associate_party = RelatedTo(Party)
     voted_for = RelatedTo(Law)
     voted_against = RelatedTo(Law)
-    member_follows = RelatedTo(ElectedOfficial)
     laws_tagged = RelatedTo(Law)
 
     @classmethod
     def createUser(cls, graph, token, job, birthYear, residancy, involvmentLevel, party):
         user = cls()
         user.token = token
-        user.birthYear = birthYear
-        user.involvmentLevel = involvmentLevel
+        user.birth_year = birthYear
+        user.involvment_level = involvmentLevel
         user.associate_party = Party.select(graph, primary_value=party).first()
-        graph.begin(autocommit=True).push(user)
+        user.job_category = JobCategory.select(graph, primary_value=job).first()
+        user.residency = Residency.select(graph, primary_value=residancy).first()
+        trans = graph.begin()
+        graph.push(user)
+        trans.commit()
         return user
+
+    def getUserAge(self):
+        curr_year = datetime.datetime.now().year
+        return curr_year - int(self.birth_year)
+
+    def changeInvlovmentLevel(self, graph, involvment_level):
+        self.involvment_level = involvment_level
+        graph.begin(autocommit=True)
+        graph.push(self)
+        return True
+
+    def changeJobField(self, graph, job):
+        self.job_category = job
+        graph.begin(autocommit=True)
+        graph.push(self)
+        return True
+
+    def changeAssociateParty(self, graph, party):
+        self.associate_party = Party.select(graph=graph, primary_value=party).first()
+        graph.begin(autocommit=True)
+        graph.push(self)
+        return True
+
+    def voteLaw(self, graph, law_name, is_upvote=True):
+        law = Law.select(graph, primary_value=law_name).first()
+        if is_upvote:
+            self.voted_for.add(law)
+        else:
+            self.voted_against.add(law)
+        graph.begin(autocommit=True)
+        graph.push(self)
+
+    def tagLaw(self, graph, law_name, tags_names):
+        law = Law.select(graph=graph, primary_value=law_name).first()
+        for tag_name in tags_names:
+            if tag_name != BLANK_TAG:
+                law.    
+
+
 
     def __str__(self, *args, **kwargs):
         return self.__ogm__.node.__str__()
