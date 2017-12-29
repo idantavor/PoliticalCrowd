647c6792664cdd5e21b2d70bc34187bea0fffcb5
ramhillel <ramhillel12@gmail.com>
2017-12-29 18:02:25 +0200

    .

--- src/modules/backend/bl/PartyService.py
+++ src/modules/backend/bl/PartyService.py
@@ -8,11 +8,10 @@ logger = logging.getLogger(__name__)
 
 NUM_OF_PROPOSALS = "num_of_proposals"
 ELECTED_PROPOSALS = "elected_proposals"
-PARTY_EFFICIENCY = "Party Efficiency"
-MEMBER_EFFICIENCY = "Memeber Efficiency"
-PARTY_MISSING = "Party Missing"
-MEMBER_MISSING = "Member Missing"
-
+PARTY_EFFICIENCY = "party_efficiency"
+MEMBER_EFFICIENCY = "memeber_efficiency"
+PARTY_MISSING = "party_missing"
+MEMBER_MISSING = "member_missing"
 
 def _getPartyEfficiancy(graph, party, laws):
     num_of_members = len(party.party_members)
@@ -30,6 +29,9 @@ def _getPartyEfficiancy(graph, party, laws):
                 member_efficiency[elect.name] = member_efficiency[elect.name] + 1
             else:
                 member_efficiency[elect.name] = 1
+
+    for name in member_efficiency.keys():
+        member_efficiency[name] = member_efficiency[name]/float(real_num_of_votes)
 
     party_efficiency = {PARTY_EFFICIENCY: real_num_of_votes / float(wanted_num_of_votes), MEMBER_EFFICIENCY: member_efficiency}
 
@@ -51,17 +53,15 @@ def getAllPartiesEfficiencyByTag(graph, tag, num_of_laws_backward):
 
     return parties_efficiancy
 
-
+# TODO debug smart
 def getAllLawProposalPerParty(graph, tag, num_of_laws_backward):
     laws = LawService.getNumOfLawsByTag(graph=graph, tag=tag, num_of_laws=num_of_laws_backward)
-    proposed_by = [law.proposed_by[0] for law in laws] # list of ElectedOfficial
-
-    logger.debug(f"Proposals for laws are:{str(proposed_by)}. check for sanity:{CommonUtils.getSingleItemInSet(proposed_by[0].member_of_party).name}")
+    proposed_by = [e for law in laws for e in list(law.proposed_by)]
 
     total_num_of_laws = len(laws)
     all_proposals = dict()
 
-    for party, elected_officals in itertools.groupby(proposed_by, key=lambda elected: CommonUtils.getSingleItemInSet(elected.member_of_party).name):
+    for party, elected_officals in itertools.groupby(proposed_by, key=lambda elected: list(elected.member_of_party)[0].name):
         elected_list = list(elected_officals)
         num_of_proposals = len(elected_list)
         elected_proposals = {name : len(list(group)) for name, group in itertools.groupby(elected_list, key=lambda elected: elected.name)}
@@ -83,6 +83,9 @@ def _getPartyMissingFromLaws(graph, party, laws):
             else:
                 member_missing[elector.name] = 1
 
+    for name in member_missing.keys():
+        member_missing[name] = member_missing[name]/float(total_missing)
+
     party_missing = {PARTY_MISSING: total_missing/float(total), MEMBER_MISSING: member_missing}
 
     return party_missing
@@ -98,6 +101,13 @@ def absentFromVotesByParty(graph, tag, num_of_laws_backward):
     logger.debug(f"Parties absent: {str(parties_missing)}")
 
     return parties_missing
+
+
+def createGeneralStats():
+    with open("Tags.txt", "r") as tags_file:
+        for tag_name in tags_file:
+            party_efficiency = f"PartyEfficiancy_{tag_name}"
+            currNode = GeneralInfo()
 
 
 
