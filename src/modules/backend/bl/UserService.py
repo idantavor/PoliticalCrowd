5f786cba1aeb154f6bcca0a186b482ca0a0cc343
oferh <ofer.handel@gmail.com>
2018-01-12 12:29:08 +0200

    123123

--- src/modules/backend/bl/UserService.py
+++ src/modules/backend/bl/UserService.py
@@ -4,7 +4,7 @@ import logging
 
 from src.modules.backend.bl import LawService
 from src.modules.backend.common.APIConstants import AgeRange, JOB_FOR, JOB_AGAINST, RESIDENT_FOR, RESIDENT_AGAINST, \
-    AGE_FOR, AGE_AGAINST, SAME, DIFF, MEMBER_ABSENT, AGE
+    AGE_FOR, AGE_AGAINST, SAME, DIFF, MEMBER_ABSENT, AGE, RESIDENCY, JOB
 from src.modules.dal.graphObjects.graphObjects import *
 import datetime
 from itertools import groupby
@@ -49,8 +49,8 @@ def getUserAgeRange(user_node):
 def getPersonalDetails(graph, user_id):
     user = User.safeSelect(graph=graph, token=user_id)
     details = dict()
-    details[WORK_AT] = list(user.work_at)[0].name
-    details[RESIDING] = list(user.residing)[0].name
+    details[JOB] = list(user.work_at)[0].name
+    details[RESIDENCY] = list(user.residing)[0].name
     details[AGE] = getUserAgeRange(user)
     return details
 
