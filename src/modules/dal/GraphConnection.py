1748c9c05dca0dfcf06b9cdfacd00e1506087187
idantavor <i.tavor@gmail.com>
2018-01-16 09:37:46 +0200

    Merge branch 'master' of https://github.com/idantavor/PoliticalCrowd fixing missing vote # Conflicts: #	src/modules/backend/bl/ProfileService.py

--- src/modules/dal/GraphConnection.py
+++ src/modules/dal/GraphConnection.py
@@ -2,8 +2,8 @@ from py2neo import Graph
 
 
 def bolt_connect():
-    return Graph("bolt://104.196.62.104:7687/db/data/",password="bibikiller")
+    return Graph("bolt://127.0.0.1:7687/db/data/",password="bibikiller")
     #return Graph("bolt://127.0.0.1:7687/db/data/", password="bibikiller")
 
 def http_connect():
-    return Graph("http://104.196.62.104:7474/db/data/",password="bibikiller")
+    return Graph("http://127.0.0.1:7474/db/data/",password="bibikiller")
